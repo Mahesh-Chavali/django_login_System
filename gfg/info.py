1d36d811982e576f6b84b21e15d7d26379184e42
Mahesh <114407586+Mahesh-Chavali@users.noreply.github.com>
2023-09-06 11:44:13 +0530

    Update info.py

--- gfg/info.py
+++ gfg/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'maheshdjango0@gmail.com'
-EMAIL_HOST_PASSWORD = 'dyiidblizbsabfya'
+EMAIL_HOST_USER = 'give your mail id for sending mail'
+EMAIL_HOST_PASSWORD = ' mail password'
 EMAIL_PORT = 587
